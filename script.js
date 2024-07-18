e03f2ce056af2b0a73121b3bc2d4c83f0da2b5df
Sam <samuelkebbell@gmail.com>
2024-07-18 16:21:52 +0100

    Updates

--- script.js
+++ script.js
@@ -195,6 +195,14 @@ btnLogin.addEventListener('click', function (e) {
 
 });
 
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+  inputTransferAmount.value = inputTransferTo.value = '';
+
 
 
 
