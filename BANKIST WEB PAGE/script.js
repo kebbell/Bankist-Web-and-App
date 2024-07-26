4044f65c619e1e9797fcfa19f11502aa915dcdb4
Sam <samuelkebbell@gmail.com>
2024-07-26 15:35:02 +0100

    UPDATES

--- BANKIST WEB PAGE/script.js
+++ BANKIST WEB PAGE/script.js
@@ -119,4 +119,20 @@ logo.classList.contains('c'); // not includes!
 logo.classList.replace('c', 'k');
 */
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  ); 
+});
