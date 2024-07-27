4fe498b74d0b4fd16797c1b4cc266db7a69204c1
Sam <samuelkebbell@gmail.com>
2024-07-27 09:11:07 +0100

    UPDATES

--- BANKIST WEB PAGE/script.js
+++ BANKIST WEB PAGE/script.js
@@ -179,10 +179,27 @@ btnScrollTo.addEventListener('click', function (e) {
 // }, 3000);
 
 // EVENT PROPAGATION IN PRACTICE
-
+// RANDOM COLOUR
 const randomInt = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
+// ^ A RANDOM INTEGER FUNCTION ^ //
 const randomColor = () =>
   `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
 
-console.log(randomColor());
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+});
+
+
