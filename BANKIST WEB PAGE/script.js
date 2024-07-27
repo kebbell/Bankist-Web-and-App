2a5088070b1e4e257d1717b82e5e6002d555ea2c
Sam <samuelkebbell@gmail.com>
2024-07-27 10:30:37 +0100

    UPDATES

--- BANKIST WEB PAGE/script.js
+++ BANKIST WEB PAGE/script.js
@@ -8,6 +8,11 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+
 
 ///////////////////////////////////////
 // Modal window
@@ -141,7 +146,30 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+///////////////////////////////////////
+// TAB COMPONENT
 
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  console.log(clicked.dataset.tab);
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+// ^ ADDING AND REMOVING CLASSES TO MANIPULATE THE PAGE ^
 
 
 
