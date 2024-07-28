0366970b4cdb92c70924ba7f5c452fb6e5d1e92f
Sam <samuelkebbell@gmail.com>
2024-07-28 10:48:28 +0100

    UPDATES

--- BANKIST WEB PAGE/script.js
+++ BANKIST WEB PAGE/script.js
@@ -238,6 +238,22 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+///////////////////////////////////////
+// REVEAL SECTIONS ON SCROLL
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 
 
 ///////////////////////////////////////
