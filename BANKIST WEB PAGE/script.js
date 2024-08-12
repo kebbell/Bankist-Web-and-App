d68729c8eb58c6dcaa28d29670519db829b671c7
Sam <samuelkebbell@gmail.com>
2024-08-12 14:09:04 +0100

    UPDATES

--- BANKIST WEB PAGE/script.js
+++ BANKIST WEB PAGE/script.js
@@ -536,3 +536,26 @@ logo.classList.replace('c', 'k');
 // [...h1.parentElement.children].forEach(function (el) {
 //   if (el !== h1) el.style.transform = 'scale(0.5)';
 // });
+
+
+///////////////////////////////////////
+// Lifecycle DOM Events
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded', e);
+});
+
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
+
+window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  console.log(e);
+  return 'Are you sure you want to leave this page?';
+});
